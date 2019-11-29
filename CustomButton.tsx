10d6cc90ac79b4bccaef9e7a58260f28615dbb6d
priya-9423 <gatpriyanka1293@gmail.com>
2019-11-29 10:40:43 +0530

    1 file modified

--- CustomButton.tsx
+++ CustomButton.tsx
@@ -5,6 +5,7 @@ import React from "react";
 //   <button onClick={props.clickEvent}> Click Me! </button>);
 // };
 
+//This syntx when () used no return is required.
 export default props => (
   <button onClick={props.clickEvent}> Click Me! </button>
   );
