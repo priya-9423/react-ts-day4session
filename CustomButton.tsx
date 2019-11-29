afb70b3afa1a3ae8a3b230916d63a1365197a39a
priya-9423 <gatpriyanka1293@gmail.com>
2019-11-29 10:46:51 +0530

    1 file modified

--- CustomButton.tsx
+++ CustomButton.tsx
@@ -1,11 +1,18 @@
 import React from "react";
 
-// export default props => {
-//   return(
-//   <button onClick={props.clickEvent}> Click Me! </button>);
-// };
+export default props => {
+
+  function clickThis()
+  {
+    alert("this also clicked");
+  }
+
+//When need more event click on same button click
+  return(
+  <button onClick={(event) => {clickThis(); props.clickEvent()}}> Click Me! </button>);
+};
 
 //This syntx when () used no return is required.
-export default props => (
-  <button onClick={props.clickEvent}> Click Me! </button>
-  );
+// export default props => (
+//   <button onClick={props.clickEvent}> Click Me! </button>
+//   );
